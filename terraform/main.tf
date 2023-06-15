1d1c8900e2e09cd4afce3d773866dc55f5431404
iwashi623 <wonderland.7878@gmail.com>
2023-06-15 14:25:29 +0900

    terraform

---
+++ terraform/main.tf
@@ -0,0 +1,21 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+    }
+  }
+}
+
+provider "aws" {
+  region = "ap-northeast-1"
+  default_tags {
+    tags = {
+      "createdBy" = "terraform apply"
+    }
+  }
+}
+
+resource "aws_s3_bucket" "test_by_terraform" {
+  bucket = "test-by-terraform"
+  force_destroy = true
+}
